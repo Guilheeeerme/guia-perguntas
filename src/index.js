50183c806e5ef93bc5ba383db93277358e2f7deb
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-10-08 20:06:03 -0300

    add Conexão MySQL, EJS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 const express = require("express");
 const app = express();
+
+const connection = require("./database/database");
+
+// Conexão MySQL
+connection
+  .autheticate()
+  .then(() => {
+    console.log("Conexão feita com o MySQL");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server connected at http://localhost");
+});
