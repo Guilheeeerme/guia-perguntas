4a30776c55fdec9567c32e5571c7e0b5c4b59c65
Guilherme Moura <61475431+Guilheeeerme@users.noreply.github.com>
2020-10-09 20:54:05 -0300

    Update routerPerguntas.js

--- src/routes/routerPerguntas.js
+++ src/routes/routerPerguntas.js
@@ -5,12 +5,8 @@ const Controller = require("../controllers/controller");
 
 router.get("/", Controller.findAllPerguntas);
 router.get("/pergunta/:id", Controller.getByID);
-
-router.get("/perguntar", (req, res) => {
-  res.render("perguntar");
-});
+router.get("/perguntar", (req, res) => { res.render("perguntar"); });
 router.post("/salvarpergunta", Controller.postSalvarPergunta);
-
 router.post("/responder", Controller.postResposta);
 
 module.exports = router;
